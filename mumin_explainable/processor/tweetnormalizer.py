9fd30a7542b244b7ee8ed807430a4809395c96a5
Vítor Lourenço <vitornaslourenco@gmail.com>
2023-08-14 13:45:05 +0100

    feat: countable explainability

---
+++ mumin_explainable/processor/tweetnormalizer.py
@@ -0,0 +1,46 @@
+from emoji import demojize
+from nltk.tokenize import TweetTokenizer
+
+tokenizer = TweetTokenizer()
+
+def normalizeToken(token):
+    lowercased_token = token.lower()
+    
+    if token.startswith("@"):
+        return "@USER"
+    elif lowercased_token.startswith("http") or lowercased_token.startswith("www"):
+        return "HTTPURL"
+    elif token == "’":
+        return "'"
+    elif token == "…":
+        return "..."
+    
+    return demojize(token)
+
+def normalizeTweet(tweet):
+    tokens = tokenizer.tokenize(tweet.replace("’", "'").replace("…", "..."))
+    normTweet = " ".join([normalizeToken(token) for token in tokens])
+
+    normTweet = (
+        normTweet.replace("cannot ", "can not ")
+        .replace("n't ", " n't ")
+        .replace("n 't ", " n't ")
+        .replace("ca n't", "can't")
+        .replace("ai n't", "ain't")
+    )
+    normTweet = (
+        normTweet.replace("'m ", " 'm ")
+        .replace("'re ", " 're ")
+        .replace("'s ", " 's ")
+        .replace("'ll ", " 'll ")
+        .replace("'d ", " 'd ")
+        .replace("'ve ", " 've ")
+    )
+    normTweet = (
+        normTweet.replace(" p . m .", "  p.m.")
+        .replace(" p . m ", " p.m ")
+        .replace(" a . m .", " a.m.")
+        .replace(" a . m ", " a.m ")
+    )
+
+    return " ".join(normTweet.split())
